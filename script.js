b150a52b98fc0236ea08bf62aa215eda611f8690
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-27 11:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function firstChar(text) {
   // your code here
-	return text[0];
+	for (let i=0;i<text.length;i++){
+		if (text[i]!==" "){
+			return text[i];
+		}
+	}
+	return " ";
 }
 // Do not change the code below
 const text = prompt("Enter text:");
