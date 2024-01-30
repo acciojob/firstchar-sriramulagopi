31a32d771b52997d9c651708108e5c5ef30b1720
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-01-30 13:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function firstChar(text) {
   // your code here
-	for (let i=0;i<text.length;i++){
-		if (text[i]!==" "){
-			return text[i];
-		}
+	text=text.trim();
+	if(text.length>0){
+		return text[0];
 	}
-	return text;
+	else{
+		return '';
+	}
 }
 // Do not change the code below
 const text = prompt("Enter text:");
+
 alert(firstChar(text));
